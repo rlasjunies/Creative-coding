7fefbd7a90f44656230cf1ad05c3be6bb554f267
Richard Lasjunies <rlasjunies@gmail.com>
2021-12-23 22:17:33 +0100

    sketxh04 - adding tweakpane

--- sketches/schetck04.js
+++ sketches/schetck04.js
@@ -1,42 +1,25 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const tweakpane = require('tweakpane');
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true
 };
 
+const params = {
+  cols: 10,
+  rows: 10,
+  animate: true,
+  frame: 1
+}
 const sketch = ({
   context,
   width,
   height,
   frame
 }) => {
-  const cells = [];
-  const cols = 10;
-  const rows = 10;
-  const numCells = cols * rows;
-
-  const gridW = width * 0.8;
-  const gridH = height * 0.8;
-  const cellW = gridW / cols;
-  const cellH = gridH / rows;
-  const margX = (width - gridW) * 0.5;
-  const margY = (height - gridH) * 0.5;
-
-  for (i = 0; i < numCells; i++) {
-    const col = i % cols;
-    const row = Math.floor(i / cols);
-
-    const x = col * cellW + margX;
-    const y = row * cellH + margY;
-    const w = cellW * 0.8;
-    const h = cellH * 0.8;
-    const cx = x + w / 2;
-    const cy = y + h / 2;
-    cells.push(new Cell(x, y, w, h, cx, cy));
-  }
 
   return ({
     context,
@@ -44,6 +27,33 @@ const sketch = ({
     height,
     frame
   }) => {
+
+    const cells = [];
+    const cols = params.cols;
+    const rows = params.rows;
+    const numCells = cols * rows;
+
+    const gridW = width * 0.8;
+    const gridH = height * 0.8;
+    const cellW = gridW / cols;
+    const cellH = gridH / rows;
+    const margX = (width - gridW) * 0.5;
+    const margY = (height - gridH) * 0.5;
+
+    for (i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cellW + margX;
+      const y = row * cellH + margY;
+      const w = cellW * 0.8;
+      const h = cellH * 0.8;
+      const cx = x + w / 2;
+      const cy = y + h / 2;
+      cells.push(new Cell(x, y, w, h, cx, cy));
+    }
+
+
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
@@ -54,7 +64,9 @@ const sketch = ({
       context.translate(cell.cx, cell.cy);
 
       // return values from -1 to 1
-      const r = random.noise2D(cell.cx + frame * 10, cell.cy, 0.001);
+      //const r = random.noise2D(cell.cx + frame * 10, cell.cy, 0.001);
+      const f = params.animate ? frame : params.frame;
+      const r = random.noise3D(cell.cx, cell.cy, f * 10, 0.001);
 
       // rotation
       const angle = r * Math.PI * 0.2;
@@ -76,7 +88,6 @@ const sketch = ({
   };
 };
 
-canvasSketch(sketch, settings);
 
 class Cell {
   constructor(x, y, w, h, cx, cy) {
@@ -88,3 +99,36 @@ class Cell {
     this.cy = cy;
   }
 }
+
+
+const createPane = () => {
+  const pane = new tweakpane.Pane();
+
+  let folder;
+  folder = pane.addFolder({
+    title: 'Grid'
+  });
+  folder.addInput(params, 'cols', {
+    min: 1,
+    max: 50,
+    sep: 1
+  });
+  folder.addInput(params, 'rows', {
+    min: 1,
+    max: 50,
+    sep: 1
+  });
+
+  folder = pane.addFolder({
+    title: 'Animate'
+  });
+  folder.addInput(params, 'animate');
+  folder.addInput(params, 'frame', {
+    min: 0,
+    max: 999
+  });
+
+}
+
+createPane();
+canvasSketch(sketch, settings);
