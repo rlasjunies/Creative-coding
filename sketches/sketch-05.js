f09071ff65256dcca57b9817d4a9e72977f29bd3
Richard Lasjunies <rlasjunies@gmail.com>
2021-12-24 09:19:41 +0100

    sketch05 initiate typecanvas

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -8,38 +8,55 @@ let text = 'A';
 let fontSize = 400;
 let fontFamily = 'serif';
 
-const sketch = () => {
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({
+  context,
+  width,
+  height
+}) => {
+  const cell = 20;
+  const cols = Math.floor(width /cell);
+  const rows = Math.floor(height / cell);
+  const numCells =  cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
+
   return ({
     context,
     width,
     height
   }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
 
-    context.fillStyle = 'black';
-    context.font = `${fontSize}px ${fontFamily}`;
-    // context.baseLine = 'middle';
-    // context.textAlign = 'center';
+    typeContext.fillStyle = 'white';
+    fontSize = cols;
+    typeContext.font = `${fontSize}px ${fontFamily}`;
 
-    const metrics = context.measureText(text);
+    const metrics = typeContext.measureText(text);
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
 
-    const ctx = (width - mw) / 2 - mx;
-    const cty = (height - mh) / 2 - my;
-    context.save();
+    const ctx = (cols - mw) / 2 - mx;
+    const cty = (rows - mh) / 2 - my;
+    typeContext.save();
 
-    context.translate(ctx, cty);
+    typeContext.translate(ctx, cty);
 
-    context.beginPath();
-    context.rect(mx, my, mw, mh);
-    context.stroke();
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
 
-    context.fillText(text, 0, 0);
-    context.restore();
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore();
+
+    context.drawImage(typeCanvas,0,0);
   };
 };
 
