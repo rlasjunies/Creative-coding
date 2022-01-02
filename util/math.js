b0838522892492eeab58b22eb22bf60aa2ecaeb0
Richard Lasjunies <rlasjunies@gmail.com>
2022-01-02 08:54:02 +0100

    fix util/math.js

--- util/math.js
+++ util/math.js
@@ -35,25 +35,3 @@ function math_mapRange(value, inputMin, inputMax, outputMin, outputMax, clamp) {
   }
 }
 
-function math_mapRange (value, inputMin, inputMax, outputMin, outputMax, clamp) {
-  // Reference:
-  // https://github.com/mattdesl/canvas-sketch-util/blob/master/math.js
-  // ---
-  // Reference:
-  // https://openframeworks.cc/documentation/math/ofMath/
-  if (Math.abs(inputMin - inputMax) < EPSILON) {
-    return outputMin;
-  } else {
-    var outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
-    if (clamp) {
-      if (outputMax < outputMin) {
-        if (outVal < outputMax) outVal = outputMax;
-        else if (outVal > outputMin) outVal = outputMin;
-      } else {
-        if (outVal > outputMax) outVal = outputMax;
-        else if (outVal < outputMin) outVal = outputMin;
-      }
-    }
-    return outVal;
-  }
-}
